a8f3635a6c112d640f6edc3168b1a2dc33e12d1e
timdeluxe <5765175+timdeluxe@users.noreply.github.com>
2023-01-04 16:47:41 +0100

    Fix rubocop offenses

--- spec/spec_helper_local.rb
+++ spec/spec_helper_local.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-#
 
 require 'augeas_spec'
 
@@ -7,4 +6,3 @@ require 'augeas_spec'
 $LOAD_PATH.unshift(File.join(__dir__, 'fixtures/modules/augeasproviders_core/lib'))
 $LOAD_PATH.unshift(File.join(__dir__, 'fixtures/modules/host_core/lib'))
 $LOAD_PATH.unshift(File.join(__dir__, 'fixtures/modules/mailalias_core/lib'))
-
